41aecdc01be001e87925eaa728a5fadbec374d33
Jose Cuapio <152364064+10JoseCuapio@users.noreply.github.com>
2025-05-01 00:14:12 -0600

    Index

--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from 'react';
 function Navegation () {
 
     return (
-        <section className='relative flex flex-col justify-center items-center h-screen'>
-            <div className='w-full flex justify-end p-6 px-14 absolute top-0 right-0'>
+        <div className='flex flex-col justify-center items-center h-screen relative m-0 p-0'>
+            <div className='w-full flex justify-end absolute top-10 right-10'>
                 <nav>
                     <ul className="flex gap-10">
                         <li><a href="">Inicio</a></li>
@@ -14,21 +14,25 @@ function Navegation () {
                     </ul>
                 </nav>
             </div>
-            <div className='flex justify-center flex-grow mt-[-100px] items-center gap-40'>
+            <div className='flex justify-center items-center gap-10'>
                 <div>
-                    <p className='text-4xl'>Hola</p>
-                    <p className='text-4xl'>Soy Jose Jaime</p>
-                    <p className='text-4xl'>Frontend developer JR</p>
-                    <button className='w-40 h-12 flex justify-center items-center rounded-xl mt-10 bg-[#3EB489] text-1xl absolute'>
-                        <img className='w-10' src="./Descargar.png" alt="" />
-                        Descargar CV
-                    </button>
+                    <div className='text-4xl'>
+                        <p>Hola</p>
+                        <p>Soy Jose Jaime</p>
+                        <p>Frontend Developer JR</p>
+                    </div>
+                    <div className='flex items-center mt-10 absolute'>
+                        <button className='flex justify-center items-center text-1xl rounded-xl bg-[#3EB489] p-2 gap-1'>
+                            Descargar CV
+                            <img className='w-10' src="./Descargar.png" alt="" />
+                        </button>
+                    </div>
                 </div>
-                <div>
-                    <img className='w-36' src="./vite.svg" alt="" />
+                <div className='grid justify-center'>
+                    <img className='w-48' src="./vite.svg" alt="" />
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
 
