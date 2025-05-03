22d3472333f9f4749828f252980a75c8240b7a30
Jose Cuapio <152364064+10JoseCuapio@users.noreply.github.com>
2025-05-03 16:49:22 -0600

    Index

--- src/components/My.jsx
+++ src/components/My.jsx
@@ -1,69 +1,7 @@
 function My () {
     return (
-        <section className="flex flex-col justify-center items-center min-h-screen">
-            <div className="flex flex-col justify-center gap-10 p-12 -mt-14">
-                <h1 className="text-4xl">Sobre mi...</h1>
-                <div className="md:flex justify-center items-center gap-10">
-                    <div className="grid justify-center">
-                        <img className="w-24" src="./vite.svg" alt="" />
-                    </div>
-                    <div className="mt-10 md:mt-0">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean maximus <br />
-                            Mauris interdum felis eget tortor posuere posuere convallis vel tortor.<br />
-                            Nam vulputate ultrices mi sit amet posuere. Sed interdum orci ac mauris <br />
-                        </p>
-                    </div>
-                </div>
-            </div>
-           
-            <div className="flex flex-col gap-10 p-12">
-                <h1 className="text-4xl mt-20"> Experencia con... </h1>
-                <div>
-                    <div className="flex justify-center gap-5 sm:gap-28 md:gap-52">
-                        <div className="flex flex-col justify-center items-center">
-                            <img className="w-20" src="./html.png" alt="" />
-                            <p>HTML</p>
-                        </div>
-                        <div className="flex flex-col justify-center items-center">
-                            <img className="w-20" src="./css.png" alt="" />
-                            <p>CSS</p>
-                        </div>
-                        <div className="flex flex-col justify-center items-center">
-                            <img className="w-20" src="./js.png" alt="" />
-                            <p>JavaScript</p>
-                        </div>
-                    </div>
-                    <div className="flex justify-center gap-5 sm:gap-20 md:gap-32 mt-10">
-                        <div className="flex flex-col justify-center items-center">
-                            <img className="w-20" src="./git.png" alt="" />
-                            <p>Git</p>
-                        </div>
-                        <div className="flex flex-col justify-center items-center">
-                            <img className="w-20" src="./mysql.png" alt="" />
-                            <p>MySQL</p>
-                        </div>
-                        <div className="flex flex-col justify-center items-center">
-                            <img className="w-20" src="./bootstrap.png" alt="" />
-                            <p>Bootstrap</p>
-                        </div>
-                    </div>
-                    <div className="flex justify-center gap-5 sm:gap-28 md:gap-52 mt-10">
-                        <div className="flex flex-col justify-center items-center">
-                            <img className="w-20" src="./laravel.png" alt="" />
-                            <p>Laravel</p>
-                        </div>
-                        <div className="flex flex-col justify-center items-center">
-                            <img className="w-20" src="./react.png" alt="" />
-                            <p>React</p>
-                        </div>
-                        <div className="flex flex-col justify-center items-center">
-                            <img className="w-20" src="./tailwind.png" alt="" />
-                            <p className="text-center" >Tailwind CSS</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <section>
+            
         </section>
     )
 }
--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -14,60 +14,57 @@ function Navegation () {
     return (
         <main className=' overflow-x-hidden'>
             <section className='flex flex-col justify-center items-center min-h-screen relative m-0 p-0 overflow-x-hidden'>
-            <div className='flex justify-end items-center text-1xl absolute top-5 right-5 z-20'>
-                <button onClick={toggleMenu} className='block sm:hidden'>
-                    <img className='w-10' src="./Menu.png" alt="" />
-                </button>
-            </div>
-
-            <div className={`sm:hidden w-full h-screen flex flex-col items-center bg-[#ECF8F3] absolute z-10 origin-right transition-transform duration-300 ease-in-out
-                ${open ? 'translate-x-0' : '-translate-x-full'}`}>
-
-                <button className='w-48 h-12 flex justify-center items-center bg-[#65c3A1] mt-20 rounded-xl gap-5'>
-                    <img className='w-7' src="./home.png" alt="" />
-                    <p className='text-2xl'>Inicio</p>
-                </button>
-
-            </div>
-            
-            <div className='hidden sm:flex justify-end items-center text-1xl absolute top-10 right-10'>
-                <nav>
-                    <ul className="flex gap-10">
-                        <li><a href="">Inicio</a></li>
-                        <li><a href="">Acerca de</a></li>
-                        <li><a href="">Servicios</a></li>
-                        <li><a href="">Contacto</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div className='flex flex-col'>
-                <div className='sm:flex justify-center items-center text-center sm:text-start gap-10'>
-                    <div>
-                        <div className='text-3xl'>
-                            <p>Hola</p>
-                            <p>Soy Jose Jaime</p>
-                            <p>Frontend Developer JR</p>
-                        </div>
-                    </div>
-                    <div className='flex justify-center mt-10 md:mt-0'>
-                        <img className='w-36' src="./vite.svg" alt="" />
-                    </div>
-                </div>
-                <div className='flex justify-center sm:justify-start mt-10'>
-                    <button className='flex justify-center items-center text-1xl rounded-xl bg-[#3EB489] p-2 gap-1'>
-                        Descargar CV
-                        <img className='w-7' src="./Descargar.png" alt="" />
+                <div className='flex justify-end items-center text-1xl absolute top-5 right-5 z-20'>
+                    <button onClick={toggleMenu} className='block md:hidden'>
+                        <img className='w-10' src="./Menu.png" alt="" />
                     </button>
                 </div>
-            </div>
-        </section>
 
-        <section className="min-h-screen flex flex-col justify-center items-center">
-    <My />
-  </section>
+                <div className={`md:hidden fixed top-0 left-0 w-full h-screen flex flex-col items-center bg-[#ECF8F3] z-10 transition-transform duration-300 ease-in-out
+                    ${open ? 'translate-x-0' : '-translate-x-full'}`}>
 
-        </main>
-        
+                    <button className='w-48 h-12 flex justify-center items-center bg-[#65c3A1] mt-20 rounded-xl gap-5'>
+                        <img className='w-7' src="./home.png" alt="" />
+                        <p className='text-2xl'>Inicio</p>
+                    </button>
+                </div>
+            
+                <div className='hidden md:flex justify-end items-center text-1xl absolute top-10 right-10'>
+                    <nav>
+                        <ul className="flex gap-10">
+                            <li><a href="">Inicio</a></li>
+                            <li><a href="">Acerca de</a></li>
+                            <li><a href="">Servicios</a></li>
+                            <li><a href="">Contacto</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className='flex flex-col'>
+                    <div className='sm:flex justify-center items-center text-center sm:text-start gap-10'>
+                        <div>
+                            <div className='text-3xl'>
+                                <p>Hola</p>
+                                <p>Soy Jose Jaime</p>
+                                <p>Frontend Developer JR</p>
+                            </div>
+                        </div>
+                        <div className='flex justify-center mt-10 md:mt-0'>
+                            <img className='w-36' src="./vite.svg" alt="" />
+                        </div>
+                    </div>
+                    <div className='flex justify-center sm:justify-start mt-10'>
+                        <button className='flex justify-center items-center text-1xl rounded-xl bg-[#3EB489] p-2 gap-1'>
+                            Descargar CV
+                            <img className='w-7' src="./Descargar.png" alt="" />
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+            <section className="flex flex-col justify-center items-center">
+                <My />
+            </section>
+        </main> 
     )
 }
 
